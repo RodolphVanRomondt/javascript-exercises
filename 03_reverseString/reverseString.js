6db42c1c987ebfcd38abf4452780e7f910ed0f1c
Rodolph VAN ROMONDT <mr.rodolph@gmail.com>
2022-12-07 13:30:52 -0400

    Did exercise reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let resultat = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        resultat += str[i]
+    }
+
+    return resultat;
 };
 
 // Do not edit below this line
