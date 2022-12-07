a775ca011b47d5f5f35d2412026e0bb73fd4366c
Rodolph VAN ROMONDT <mr.rodolph@gmail.com>
2022-12-07 13:22:20 -0400

    Did exercise repeatingString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    if (num == 0 || str == "") {
+        return "";
+    }
 
+    let resultat = "";
+
+    for (let i = 0; i < num; i++) {
+        resultat += str;
+    }
+
+    return resultat;
 };
 
 // Do not edit below this line
