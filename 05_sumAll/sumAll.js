4488d1bfd3325e428733b731056fa1446728b427
Rodolph VAN ROMONDT <mr.rodolph@gmail.com>
2022-12-12 13:53:47 -0400

    Did exercise sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let total = 0;
 
+    if (x < 0 || y < 0 || typeof(x) !== "number" || typeof(y) !== "number") {
+        return "ERROR";
+    }
+
+    if (x < y) {
+        for (x; x <= y; x++) {
+            total += x;
+        }
+    } else if (x > y) {
+        for (y; y <= x; y++) {
+            total += y;
+        }
+    } else {
+        return x;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
