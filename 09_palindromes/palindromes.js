65f35153f233cff578bad1b306f6436607affbc6
Rodolph VAN ROMONDT <mr.rodolph@gmail.com>
2022-12-15 22:44:48 -0400

    Did exercise palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    string = string.toLowerCase();
+    let palindrome = "";
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        if ((/[a-z]/).test(string[i])) {
+            palindrome += string[i];
+        }
+    }
+
+    return palindrome == palindrome.split("").reverse().join("");
 };
 
 // Do not edit below this line
